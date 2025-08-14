3409810ee4485a30f962b447562e6c6988e95699
Mathias Stadler <stadler-mathias@web.de>
2025-08-14 14:37:50 +0200

    save

--- tests/05_main.rs
+++ tests/05_main.rs
@@ -1,18 +1,8 @@
 use assert_cmd::Command;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 
     #[test]
     fn cli_test() {
