f25bbad4d2dd557b4e29c502935a59151d0cc461
Mathias Stadler <stadler-mathias@web.de>
2025-08-14 14:50:07 +0200

    save

--- tests/05_main.rs
+++ tests/05_main.rs
@@ -1,24 +1,8 @@
 use assert_cmd::Command;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn cli_test() {
-        let mut cmd = Command::cargo_bin("rust_main_startup").unwrap();
-        cmd.assert().success();
-    }
 
     #[test]
     fn cli_output() {
@@ -26,4 +10,13 @@ mod tests {
 
         cmd.assert().success().stdout("Hello, world!\n");
     }
+
+    #[test]
+    fn cli_output_fail() {
+        let mut cmd = Command::cargo_bin("rust_main_startup").unwrap();
+
+        cmd.assert().success().stdout("Failed Hello, world!\n");
+    }
 }
+
+// cargo test --test 05_main
